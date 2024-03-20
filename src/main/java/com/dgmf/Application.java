c23d50b27ce229cc893b2fcf067ea4a9150c4c94
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-03-20 12:48:49 +0100

    Enable Spring Security 6 on Top of Main Application Class

--- src/main/java/com/dgmf/Application.java
+++ src/main/java/com/dgmf/Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+// To Disable Spring Security 6
+// @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class Application {
 
